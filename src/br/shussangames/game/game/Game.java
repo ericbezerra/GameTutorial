e5a6ec91c863689721115af792d2dafc7035332a
eric shussan <ericshussan@hotmail.com>
2014-09-16 15:26:26 -0300

    fmu game integrated project

---
+++ src/br/shussangames/game/game/Game.java
@@ -0,0 +1,25 @@
+package br.shussangames.game.game;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Game extends JFrame implements KeyListener{
+	
+	
+
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
