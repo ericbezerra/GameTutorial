21908d5f6b3eae34fb631ee7b54c27139a32ceca
eric shussan <ericshussan@hotmail.com>
2014-09-22 17:23:46 -0300

    Cherno Game Tutorial.

--- src/br/shussangames/game/game/Game.java
+++ src/br/shussangames/game/game/Game.java
@@ -1,99 +1,121 @@
 package br.shussangames.game.game;
 
-import java.awt.Color;
+import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
+import br.shussangames.game.game.graphics.Screen;
+
 @SuppressWarnings("serial")
-public class Game extends JFrame implements KeyListener{
+public class Game extends Canvas implements Runnable {
+	public static int width = 300;
+	public static int height = width / 16 * 9;
+	public static int scale = 3;
+	public static String title = "Rain";
 	
-	private final int WIDTH = 200;
-	private final int HEIGHT = 150;
-	private final int SCALE = 3;
-	private Dimension dimension = new Dimension(WIDTH * SCALE, HEIGHT * SCALE);
 	private Thread thread;
-	
-	BufferedImage backBuffer;
-	char teclaPressionada;
-	int velocidade = 1;
-	int x = 200;
-	int y = 200;
-	boolean running = true;
-	
-	public static void main(String[] args) {
-		Game game = new Game();
-		game.run();
-	}
-	
-	public void init(){
-		setSize(dimension);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLayout(null);
-		setVisible(true);
-		setResizable(false);
-		setLocationRelativeTo(null);
-		backBuffer = new BufferedImage(WIDTH * SCALE, HEIGHT * SCALE, BufferedImage.TYPE_INT_RGB);
-		addKeyListener(this);
-		
-	}
-	
-	public void paint(){
-		Graphics g = getGraphics();
-		Graphics bbg = backBuffer.getGraphics();
-		bbg.setColor(Color.BLUE);
-		bbg.fillRect(0, 0, WIDTH * SCALE, HEIGHT * SCALE);
-		bbg.setColor(Color.YELLOW);
-		bbg.fillOval(x, y, 50, 50);
-		g.drawImage(backBuffer, 0, 0, this);
+	private JFrame frame;
+	private boolean running = false;
+
+	private Screen screen;
+
+	private BufferedImage image = new BufferedImage(width, height,
+			BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer())
+			.getData();
+
+	public Game() {
+		Dimension size = new Dimension(width * scale, height * scale);
+		// this will set the size of window with this Dimension size
+		setPreferredSize(size);
+		screen = new Screen(width, height);
+		frame = new JFrame();
 	}
 
-	public void run(){
-		this.init();
-		while(running){
-			try{
-			Thread.sleep(1000/30);
-			}catch(InterruptedException e){
-				e.printStackTrace();
-				stop();
-			}
-			this.paint();
-		}
+	public synchronized void start() {
+		running = true;
+		thread = new Thread(this, "display");
+		thread.start();
 	}
-	
-	public void stop(){
+
+	public synchronized void stop() {
 		running = false;
-		try{
-		thread.join();
-		}catch(Exception e){
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void keyPressed(KeyEvent e) {
-		teclaPressionada = e.getKeyChar();
-		
-		if(e.getKeyCode() == KeyEvent.VK_A){
-			x -= velocidade;
+
+	public void run() {
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		final double ns = 1000000000.0 / 60.0;
+		double delta = 0;
+		int frames = 0;
+		int updates = 0;
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1){
+				update();
+				updates++;
+				delta --;
+			}
+			render();
+			frames ++;
+			
+			if(System.currentTimeMillis() - timer > 1000){
+				timer += 1000; 
+				frame.setTitle(title+ " | " + updates + "ups, " + frames + "fps");
+				updates = 0;
+				frames = 0;
+			}
 		}
-		if(e.getKeyCode() == KeyEvent.VK_D){
-			x += velocidade;
-		}
-		if(e.getKeyCode() == KeyEvent.VK_W){
-			y -= velocidade;
-		}
-		if(e.getKeyCode() == KeyEvent.VK_S){
-			y += velocidade;
-		}
+		stop();
 	}
-	public void keyReleased(KeyEvent e) {
-		
+
+	public void update() {
+
 	}
-	public void keyTyped(KeyEvent e) {
-		
+
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		screen.clear();
+		screen.render();
+
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
+		}
+		Graphics g = bs.getDrawGraphics();
+		// --------------Graphics------------------
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		// --------------Graphics------------------
+		g.dispose();
+		bs.show();
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle(Game.title);
+		game.frame.add(game);
+		// this method receive the preferred size method arguments
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+
+		game.start();
 	}
 }
